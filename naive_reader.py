10f42fdb8ad109deb6c6442d39b7aed53044637a
Daniel Municio <danielmunicio360@gmail.com>
2025-05-23 19:29:36 -0700

    First Iteration Working

---
+++ naive_reader.py
@@ -0,0 +1,37 @@
+import serial
+
+
+ser = serial.Serial(
+    port='COM6',
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=1
+)
+
+def read_frame(ser):
+    if ser.read(1) != b'\x7E':
+        return None
+    length_bytes = ser.read(2)
+    length = int.from_bytes(length_bytes, byteorder='big')
+    frame_data = ser.read(length)
+    if len(frame_data) < length:
+        return None
+    return frame_data
+
+def parse_explicit_rx(frame_data):
+    if frame_data[0] != 0x91:
+        return None
+    rf_data = frame_data[18:]
+    return rf_data.decode('utf-8', errors='replace')
+
+
+print("Serial initialized")
+
+while True:
+    frame = read_frame(ser)
+    if frame:
+        rf_data = parse_explicit_rx(frame)
+        if rf_data:
+            print(f"RF Data: {rf_data}")
